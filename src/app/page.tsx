085cdd769d47c9f62ef07d62b0687769434e5585
brunonxale <brunonxale@github.com>
2025-08-06 14:50:27 -0600

    feat: initialize HomePage component with state management for Flashcard sets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
 "use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { FlashcardSet } from "@/lib/types";
+
 import HeaderSection from "@/components/Layout/HeaderSection";
 import FlashcardSetList from "@/components/Layout/FlashcardSetList";
 
 export default function HomePage() {
+  const [sets, setSets] = useState<FlashcardSet[]>([]);
+
+  const router = useRouter();
+
+  useEffect(() => {
+
+  }, []);
+
   return (
     <div className="max-w-4xl mx-auto p-6">
       <HeaderSection />
-      <FlashcardSetList/>
+      <FlashcardSetList
+        sets={sets}
+        onEdit={() => {}}
+        onDelete={() => {}}
+      />
     </div>
   );
 }

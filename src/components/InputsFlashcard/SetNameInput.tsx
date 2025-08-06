336472c5ad01b6280a8232f656433b721a941ab9
brunonxale <brunonxale@github.com>
2025-08-06 13:03:25 -0600

    feat: add error handling to SetNameInput component

--- src/components/InputsFlashcard/SetNameInput.tsx
+++ src/components/InputsFlashcard/SetNameInput.tsx
@@ -3,9 +3,10 @@
 interface SetNameInputProps {
   value: string;
   onChange: (value: string) => void;
+  error?: string;
 }
 
-export default function SetNameInput({ value, onChange}: SetNameInputProps) {
+export default function SetNameInput({ value, onChange, error }: SetNameInputProps) {
   return (
     <div className="mb-4">
       <input
@@ -13,8 +14,11 @@ export default function SetNameInput({ value, onChange}: SetNameInputProps) {
         placeholder="Name of the Set *"
         value={value}
         onChange={(e) => onChange(e.target.value)}
-        className={`w-full p-3 border rounded text-lg`}
+        className={`w-full p-3 border rounded text-lg ${
+          error ? "border-red-500" : "border-gray-300"
+        }`}
       />
+      {error && <p className="text-red-600 mt-1 text-sm">{error}</p>}
     </div>
   );
 }

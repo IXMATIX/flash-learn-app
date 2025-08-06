7f96f056ae0c408f80c8d6943681d543755fd17b
brunonxale <brunonxale@github.com>
2025-08-06 13:03:10 -0600

    feat: add props for value and onChange handling in SetNameInput component

--- src/components/InputsFlashcard/SetNameInput.tsx
+++ src/components/InputsFlashcard/SetNameInput.tsx
@@ -1,11 +1,19 @@
+// components/SetNameInput.tsx
 
-export default function SetNameInput({ }){
+interface SetNameInputProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export default function SetNameInput({ value, onChange}: SetNameInputProps) {
   return (
     <div className="mb-4">
       <input
         type="text"
         placeholder="Name of the Set *"
-        className="w-full p-3 border rounded text-lg"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        className={`w-full p-3 border rounded text-lg`}
       />
     </div>
   );

607e8f4a71e82d434751bcf215d9c7e69c574e87
brunonxale <brunonxale@github.com>
2025-08-06 11:29:00 -0600

    feat: add SetDescriptionInput component to CreateFlashcardSetPage

--- src/app/flashcards/new/page.tsx
+++ src/app/flashcards/new/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import SetDescriptionInput from "@/components/InputsFlashcard/SetDescriptionInput";
 import SetNameInput from "@/components/InputsFlashcard/SetNameInput";
 
 export default function CreateFlashcardSetPage() {
@@ -6,6 +7,7 @@ export default function CreateFlashcardSetPage() {
     <div className="max-w-2xl mx-auto p-6 text-gray-800">
       <h1 className="text-3xl font-bold mb-6 text-blue-700">📚 Create a new set of flashcards</h1>
       <SetNameInput/>
+      <SetDescriptionInput/>
     </div>
   );
 }

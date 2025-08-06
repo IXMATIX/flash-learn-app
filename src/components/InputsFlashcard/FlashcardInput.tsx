58524284bf9698891e0d221ba23ad2b2848d5311
brunonxale <brunonxale@github.com>
2025-08-06 13:00:14 -0600

    feat: implement FlashcardInput component with delete functionality and type definitions

--- src/components/InputsFlashcard/FlashcardInput.tsx
+++ src/components/InputsFlashcard/FlashcardInput.tsx
@@ -1,27 +1,44 @@
 "use client";
 import { Trash2 } from "lucide-react";
+import { Flashcard } from "@/lib/types";
 
+interface FlashcardInputProps {
+  index: number;
+  card: Flashcard;
+  error?: string;
+  onChange: (field: "front" | "back", value: string) => void;
+  onDelete: () => void;
+}
 
-export default function FlashcardInput({}) {
+export default function FlashcardInput({
+  index,
+  card,
+  onChange,
+  onDelete,
+}: FlashcardInputProps) {
   return (
     <div className="mb-5 border p-4 rounded-lg bg-blue-50">
       <div className="mb-2">
         <input
           type="text"
-          placeholder="Front"
-    
-          className="w-full p-2 border rounded"
+          placeholder={`Frente #${index + 1}`}
+          value={card.front}
+          onChange={(e) => onChange("front", e.target.value)}
+          className={`w-full p-2 border rounded`}
         />
       </div>
       <div>
         <input
           type="text"
-          placeholder="Back #"
-    
-          className="w-full p-2 border rounded"
+          placeholder={`Back #${index + 1}`}
+          value={card.back}
+          onChange={(e) => onChange("back", e.target.value)}
+          className={`w-full p-2 border rounded`}
         />
       </div>
+
       <button
+        onClick={onDelete}
         className="text-red-600 hover:text-red-800 flex items-center gap-1 mt-2 text-sm"
       >
         <Trash2 className="w-4 h-4" />
